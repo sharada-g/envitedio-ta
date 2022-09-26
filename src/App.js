823885c0bb56219ba81686e854c1944b4ae2b9f5
sharada-g <gunathilakesharada@gmail.com>
2022-09-26 13:39:02 +1000

    style component set up

--- src/App.js
+++ src/App.js
@@ -1,21 +1,40 @@
 import { Routes, Route } from "react-router-dom";
 
+import { ThemeProvider } from "styled-components";
+
 // import components
 import Landing from "./pages/landing";
 import EventCreate from "./pages/event/create";
 import EventView from "./pages/event/view";
 import NotFound from "./pages/NotFound";
 
+const AppTheme = {
+  colors: {
+    primary1: "#240D57",
+    primary2: "#501FC1",
+    primary3: "#8456EC",
+    primary4: "#E87BF8",
+    secondary1: "#CCB6FF",
+    secondary2: "#EDE5FF",
+    secondary3: "#F6F2FF",
+  },
+
+  breakpoints: {
+    ipad: "960px",
+    mcbook: "1512px",
+  },
+};
+
 function App() {
   return (
-    <>
+    <ThemeProvider theme={AppTheme}>
       <Routes>
         <Route path="/" element={<Landing />} />
         <Route path="/create" element={<EventCreate />} />
         <Route path="/event" element={<EventView />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
-    </>
+    </ThemeProvider>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,15 +1,33 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
+import { createGlobalStyle } from "styled-components";
+
 import { BrowserRouter } from "react-router-dom";
 
 import App from "./App";
 
 import reportWebVitals from "./reportWebVitals";
 
+// global styles for the entire app
+const IndexStyle = createGlobalStyle`
+    body {
+    background-color: #F6F2FF;
+    margin: 0;
+    font-family: -apple-system, 'Helvetica Neue';
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    }
+
+    code {
+    font-family:  -apple-system, 'Helvetica Neue';
+    }
+    `;
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
+    <IndexStyle />
     <BrowserRouter>
       <App />
     </BrowserRouter>
